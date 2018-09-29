126005d7b8a7326af10a12076f4e713f8f306442
zaka7024 <38314622+zaka7024@users.noreply.github.com>
2018-09-29 17:08:26 +0300

    add all fragments

---
+++ app/src/main/java/whatsapp_situation/lemonlab/com/whatsapp_situation/Fragment05.kt
@@ -0,0 +1,36 @@
+package whatsapp_situation.lemonlab.com.whatsapp_situation
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.layout_01.*
+import whatsapp_situation.lemonlab.com.whatsapp_situation.data.DB_SQL_LITE
+
+/**
+ * Created by HP on 10/09/2018.
+ */
+class Fragment05() : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.layout_01,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+
+        var db = DB_SQL_LITE(context!!)
+        //db.insertData("A-1","بحبك ❤",0)
+        //db.insertData("A-2","صباح الخير يا تافه",0)
+        //db.insertData("B-1","بكرهك 🤦",0)
+        //db.insertData("B-2","تبًا لكم جميعًا",0)
+
+        super.onActivityCreated(savedInstanceState)
+        var layoutMnager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        var adapter = Adapter(context!!, db.loadDataFromDB('E'), 'E')
+
+        recyclerView_01.layoutManager = layoutMnager
+        recyclerView_01.adapter = adapter
+    }
+}
