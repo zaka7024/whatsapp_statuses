421e8de635568a1d39f5c1eeb29a7485b81e47ba
zaka7024 <38314622+zaka7024@users.noreply.github.com>
2018-10-03 08:47:25 +0300

    redesign home page

--- app/src/main/java/whatsapp_situation/lemonlab/com/whatsapp_situation/data/topic.kt
+++ app/src/main/java/whatsapp_situation/lemonlab/com/whatsapp_situation/data/topic.kt
@@ -8,63 +8,40 @@ import whatsapp_situation.lemonlab.com.whatsapp_situation.R
 class topic {
     companion object {
         var topic_data:ArrayList<String> = ArrayList<String>().apply {
-            add("حالات الاصدقاء")
-            add("حالات دينية")
-            add("حالات الى الام")
-            add("حالات ضحك")
-            add("حالات الاعياد")
-            add("حالات رمضان")
-            add("حالات الصباح")
-            add("حالات حزن")
             add("حالات رومنسية")
-            add("حالات شعر")
+            add("حالات حزن")
+            add("حالات حب")
+            add("حالات الاصدقاء")
+            add("حالات منوعة")
             add("حالات حكم")
-            add("حالات شوق")
+            add("حالات الى الام")
+            add("امثال شعبية")
+            add("حالات الصباح")
             add("حالات دعاء")
-            add("منشورات مفيدة")
-            add("منشورات مسائية")
+            add("حالات دينية")
+            add("حالات ضحك")
+            add("حالات حكم")
+            add("حالات رمضان")
             add("منشورات العيد")
-            add("منشورات مزخرفة")
-        }
+            add("منشورات مسائية")
 
-        var topic_type:ArrayList<String> = ArrayList<String>().apply {
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
-            add("A")
         }
 
         var topic_images:ArrayList<Int> = ArrayList<Int>().apply {
-           add(R.drawable.image4)
-           add(R.drawable.islam)
-           add(R.drawable.image2)
-           add(R.drawable.joke)
-           add(R.drawable.image5)
-           add(R.drawable.image3)
-           add(R.drawable.image7)
-           add(R.drawable.image8)
-           add(R.drawable.image9)
-           add(R.drawable.image12)
-           add(R.drawable.image11)
-           add(R.drawable.image10)
-           add(R.drawable.dooa)
-           add(R.drawable.information)
-           add(R.drawable.evening)
-           add(R.drawable.eid)
-           add(R.drawable.allah)
+           add(R.drawable.image9) // romantic
+           add(R.drawable.image8) // sad
+           add(R.drawable.love) // love
+           add(R.drawable.image4) // friends
+           add(R.drawable.different) // different
+           add(R.drawable.wisdom) // wisdom
+            add(R.drawable.image2) // mother
+           add(R.drawable.image7)  // morning
+           add(R.drawable.quotes) // quotes
+           add(R.drawable.dooa) // Doa'a
+           add(R.drawable.allah) // islamic
+           add(R.drawable.joke) /// joke
+           add(R.drawable.image3) // ramadan
+           add(R.drawable.eid) // eid
         }
     }
 }
